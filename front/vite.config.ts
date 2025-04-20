194952ac556bd4331d060fea5951498c347fee02
Artem <79054301+Bumer-32@users.noreply.github.com>
2025-04-20 21:01:55 +0300

    prettier

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,25 +1,25 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  build: {
-    outDir: "../back/build/resources/main/static",
-    sourcemap: true,
-  },
-  server: {
-    open: "/",
-    proxy: {
-      "/api": {
-        target: "localhost:8080",
-        changeOrigin: true,
-        configure: (proxy) => {
-          proxy.on("proxyReq", (proxyReq, _req, _res) => {
-            proxyReq.removeHeader("origin");
-          });
+    plugins: [svelte()],
+    build: {
+        outDir: "../back/build/resources/main/static",
+        sourcemap: true,
+    },
+    server: {
+        open: "/",
+        proxy: {
+            "/api": {
+                target: "localhost:8080",
+                changeOrigin: true,
+                configure: (proxy) => {
+                    proxy.on("proxyReq", (proxyReq, _req, _res) => {
+                        proxyReq.removeHeader("origin");
+                    });
+                },
+            },
         },
-      }
-    }
-  }
-})
+    },
+});
